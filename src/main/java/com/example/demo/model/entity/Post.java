ab7395bde761004e4cdc078c854420c4f12ec262
alfreddey <lilscar246@gmail.com>
2026-02-10 14:24:30 +0000

    feat: define post and user as domain entity

--- src/main/java/com/example/demo/model/entity/Post.java
+++ src/main/java/com/example/demo/model/entity/Post.java
@@ -1,17 +1,31 @@
 package com.example.demo.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
+@Document(collection = "posts")
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Post {
+    @Id
     private String id;
+    @Indexed
     private String title;
     private String content;
     private String authorEmail;
     private List<String> tags;
     private List<Comment> comments;
+
+    @CreatedDate
     private Date createdAt;
 }
--- src/main/java/com/example/demo/model/entity/User.java
+++ src/main/java/com/example/demo/model/entity/User.java
@@ -1,27 +1,14 @@
 package com.example.demo.model.entity;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
-@Setter
-@Getter
+@Document(collection = "users")
+@Data
 public class User {
-    private String id;
+    @Id private String id;
     private String name;
     private String email;
     private String password;
-
-    public User() {}
-
-    public User(String id, String name, String email, String password) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-    }
-
-    public User(String name, String email, String password) {
-        this(null, name, email, password);
-    }
-
 }
