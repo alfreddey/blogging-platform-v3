e7380bd9f5851b9fe81955ed27522f2061c47954
alfreddey <lilscar246@gmail.com>
2026-02-13 09:22:43 +0000

    feat: add graphql void scalar and config to handle void returns in graphql schema

--- src/main/java/com/example/demo/config/MongoConfig.java
+++ src/main/java/com/example/demo/config/MongoConfig.java
@@ -4,10 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
+@EnableMongoAuditing
 public class MongoConfig {
     @Bean
     MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
