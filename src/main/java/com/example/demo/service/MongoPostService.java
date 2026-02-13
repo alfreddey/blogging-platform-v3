100a183f21e59eb14fe80eaffaf311b3e35b8f23
alfreddey <lilscar246@gmail.com>
2026-02-13 09:19:55 +0000

    feat: implement post service pagination and remove in-memory caching logic

--- src/main/java/com/example/demo/service/MongoPostService.java
+++ src/main/java/com/example/demo/service/MongoPostService.java
@@ -1,11 +1,9 @@
 package com.example.demo.service;
 
-import com.example.demo.cache.Cache;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.entity.Post;
 import com.example.demo.repository.interfaces.PostRepository;
 import com.example.demo.service.interfaces.PostService;
-import com.example.demo.utils.PostSearch;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.CachePut;
@@ -21,46 +19,22 @@ import java.util.List;
 @CacheConfig(cacheNames = "posts")
 public class MongoPostService implements PostService {
     private final PostRepository postRepository;
-    private final Cache<Post> postCache;
-    private final PostSearch postSearch;
 
-    public MongoPostService(PostRepository postRepository, Cache<Post> postCache, PostSearch postSearch) {
-        this.postCache = postCache;
+    public MongoPostService(PostRepository postRepository) {
         this.postRepository = postRepository;
-        this.postSearch = postSearch;
     }
-
 
     @Override
     public List<Post> getAll(int page, int size, String sortBy) {
-        var cacheResult = postCache.getAll();
-
-        if (cacheResult != null && !cacheResult.isEmpty() && cacheResult.size() >= size) {
-            return cacheResult;
-        }
-
         var pageable = PageRequest.of(page, size, Sort.by(sortBy).ascending());
-        var posts = postRepository.findAll(pageable).getContent();
 
-        posts.forEach(postCache::put);
-
-        return posts;
+        return postRepository.findAll(pageable).getContent();
     }
 
     @Cacheable
     @Override
     public Post getById(String postId) {
-        var cacheResult = postCache.getById(postId);
-
-        if (cacheResult != null) {
-            return cacheResult;
-        }
-
-        var post = postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
-
-        postCache.put(post);
-
-        return post;
+        return postRepository.findById(postId).orElseThrow(() -> new ResourceNotFoundException("Post not found with id: " + postId));
     }
 
     @CachePut(key = "#result.id")
@@ -68,8 +42,6 @@ public class MongoPostService implements PostService {
     @Override
     public Post create(Post post) {
         post = postRepository.save(post);
-        postCache.put(post);
-
         return post;
     }
 
@@ -77,7 +49,6 @@ public class MongoPostService implements PostService {
     @Transactional
     @Override
     public void delete(String postId) {
-        postCache.remove(postId);
         postRepository.deleteById(postId);
     }
 
@@ -85,22 +56,13 @@ public class MongoPostService implements PostService {
     @Transactional
     @Override
     public Post updatePostContent(String postId, String content) {
-        var post = postRepository.updatePostContent(postId, content);
-        postCache.put(postId, post);
-
-        return post;
+        return postRepository.updatePostContent(postId, content);
     }
 
     @Cacheable(key = "#title")
     @Override
     public Post findByTitle(String title) {
-        var post = postSearch.findByTitle(title);
-
-        if (post != null) {
-            return post;
-        }
-
-        post = postRepository.findByTitle(title);
+        var post = postRepository.findByTitle(title);
 
         if (post != null) {
             return post;
