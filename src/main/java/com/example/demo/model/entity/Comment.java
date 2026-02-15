217030e89d4716c150ba0fc912a8c03400ea74c0
alfreddey <lilscar246@gmail.com>
2026-02-15 13:17:51 +0000

    feat: modify comment entity

--- src/main/java/com/example/demo/model/entity/Comment.java
+++ src/main/java/com/example/demo/model/entity/Comment.java
@@ -1,13 +1,23 @@
 package com.example.demo.model.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 
 import java.util.Date;
 
+
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Comment {
+    @Id
     private String id;
     private String authorEmail;
     private String text;
+
+    @CreatedDate
     private Date createdAt;
 }
